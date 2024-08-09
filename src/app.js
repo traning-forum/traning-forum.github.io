7674efea10b80404daeb9574f82f6483ce4dec9a
DS-JS-Dev <daniel.sarov98@gmail.com>
2024-08-09 15:46:56 +0300

    add dev tools and config

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import page from "@page/page.mjs";
 import { html, render } from "@lit/lit-html.js";
 import {classMap} from "@lit/directives/class-map.js";
 import {styleMap} from "@lit/directives/style-map.js";
-import {until} from "@lit/directives/until.js";
 import {repeat} from "@lit/directives/repeat.js";
+import {until} from "@lit/directives/until.js";
 
 
 page('/', homeView);
